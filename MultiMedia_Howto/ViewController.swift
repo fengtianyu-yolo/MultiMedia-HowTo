9896b6b1ad27a3e673ff052744cd0e4a2e40f6b3
fengtianyu <fengtianyu@bytedance.com>
2022-02-27 10:39:19 +0800

    Initial Commit

---
+++ MultiMedia_Howto/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MultiMedia_Howto
+//
+//  Created by fengtianyu on 2022/2/27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
