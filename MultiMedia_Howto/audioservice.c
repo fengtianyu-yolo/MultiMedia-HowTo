20c4a5fea7a312acde7e0b487ada6f4bf58ed086
fengtianyu <fengtianyu@bytedance.com>
2022-02-27 11:42:07 +0800

    完成音频输入设备的打开，info添加麦克风访问权限

--- MultiMedia_Howto/audioservice.c
+++ MultiMedia_Howto/audioservice.c
@@ -15,4 +15,22 @@ void greeting() {
     
     av_log_set_level(AV_LOG_DEBUG);
     av_log(NULL, AV_LOG_DEBUG, "ok! avutil success call ! \n");
+    
+    // 注册设备
+    avdevice_register_all();
+    
+    // 定义环境上下文变量
+    AVFormatContext *fmt_ctx = NULL;
+    // 定义 devicename 参数: ':0'，冒号前面表示为视频设备；冒号后面代表音频设备，0表示第一个音频设备
+    char *devicename = ":0";
+    // 定义 参数选项 参数
+    AVDictionary *options = NULL;
+    
+    // 设备的采集格式
+    AVInputFormat *input_format = av_find_input_format("avfoundation");
+    
+    int open_result = avformat_open_input(&fmt_ctx, devicename, input_format, &options);
+    if (open_result == 0) {
+        printf("设备成功打开 \n");
+    }
 }
