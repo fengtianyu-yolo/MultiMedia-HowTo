c6ea265ce200c6cd1dacc1f0eb719a8c3721e498
fengtianyu <fengtianyu@bytedance.com>
2022-02-27 11:29:31 +0800

    成功调用了 ffmpege 中的函数

--- MultiMedia_Howto/audioservice.c
+++ MultiMedia_Howto/audioservice.c
@@ -6,7 +6,11 @@
 //
 
 #include "audioservice.h"
+#include "libavutil/avutil.h"
 
 void greeting() {
-    printf("ok! you call c function success.");
+    printf("ok! you call c function success. \n");
+    
+    av_log_set_level(AV_LOG_DEBUG);
+    av_log(NULL, AV_LOG_DEBUG, "ok! avutil success call ! \n");
 }
