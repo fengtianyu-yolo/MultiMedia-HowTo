9b380a6893a94f9aaf0fb258ce97312927b0975e
fengtianyu <fengtianyu@bytedance.com>
2022-02-27 10:44:57 +0800

    feature : 完成Mac窗口的创建

--- MultiMedia_Howto/ViewController.swift
+++ MultiMedia_Howto/ViewController.swift
@@ -9,16 +9,38 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    let buttonSize = NSSize(width: 80, height: 30)
+    let frameSize = NSSize(width: 320, height: 240)
+    
+    var button: NSButton?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.setFrameSize(frameSize)
+        self.view.needsDisplay = true
 
-        // Do any additional setup after loading the view.
+        button = NSButton(title: "录音", target: self, action: #selector(buttonClicked))
+        button?.frame = NSRect(x: (frameSize.width-buttonSize.width)/2, y: (frameSize.height-buttonSize.height)/2, width: buttonSize.width, height: buttonSize.height)
+        button?.setButtonType(.pushOnPushOff)
+        if let btn = button {
+            view.addSubview(btn)
+        }
+        
+        view.window?.center()
+        
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    // MARK: - Event Action
+    
+    @objc func buttonClicked() {
+        print("record ")
     }
 
 
