2c1ca0272b6092da37cf481c10177d042959eabd
fengtianyu <fengtianyu@bytedance.com>
2022-02-27 11:32:16 +0800

    导入设备相关头文件

--- MultiMedia_Howto/audioservice.c
+++ MultiMedia_Howto/audioservice.c
@@ -7,6 +7,8 @@
 
 #include "audioservice.h"
 #include "libavutil/avutil.h"
+#include "libavdevice/avdevice.h"
+#include "libavformat/avformat.h"
 
 void greeting() {
     printf("ok! you call c function success. \n");
